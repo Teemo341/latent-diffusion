838dcaf057be697b689759501bf2e610ed353e9b
341_on_node_6 <838839085@qq.com>
2024-11-15 16:23:01 +0800

    Update training scripts and configurations for Salinas_Corrected dataset

--- train_VCA.sh
+++ train_VCA.sh
@@ -9,4 +9,4 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:1
 
-python -u -m ldm.models.hsi_encoder  --data Pavia --save_path ./models/first_stage_models/HSI/VCA/ --feature_channels 9 --seed 0 --visual_channels 46 27 10
+python -u -m ldm.models.hsi_encoder  --data Salinas_Corrected --save_path ./models/first_stage_models/HSI/VCA/ --feature_channels 16 --seed 0 --visual_channels 36 17 11
