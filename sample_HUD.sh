afdbe9cd64c9598e0066f512b44c6925afd92396
341_on_node_6 <838839085@qq.com>
2024-12-19 16:14:04 +0800

    ablation KSC

--- sample_HUD.sh
+++ sample_HUD.sh
@@ -10,7 +10,7 @@
 #SBATCH --gres=gpu:1
 
 python -u -m scripts.sample_diffusion_HSI \
-    --dataset Indian_Pines_Corrected \
+    --dataset KSC_Corrected \
     --resume ./models/HUD \
     --n_samples 8 \
     --eta 1.0 \
