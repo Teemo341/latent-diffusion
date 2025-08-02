582eb22c855aace1e3cca7fc610709fb42929526
ssy <838839085@qq.com>
2025-08-02 18:06:24 +0800

    Add new experiment scripts for classifier, evaluation, and various models

---
+++ shell/train_MSTPlus.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#SBATCH -J MSTPlus
+#SBATCH -o ./results/MSTPlus.out               
+#SBATCH -p compute1             
+#SBATCH -A compute1    
+#SBATCH --qos=compute1               
+#SBATCH -N 1               
+#SBATCH --ntasks-per-node=1                    
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+
+
+python -u -m experiments.models.MSTPlus  --datasets Indian_Pines_Corrected
+# python -u -m experiments.models.MSTPlus1  --datasets KSC_Corrected
+# python -u -m experiments.models.MSTPlus2  --datasets KSC_Corrected
+# python -u -m experiments.models.MSTPlus3  --datasets KSC_Corrected
+# python -u -m experiments.models.MSTPlus4  --datasets KSC_Corrected
